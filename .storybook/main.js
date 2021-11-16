ddabe960ba400394e70be7170d87cc8461536e65
codyNog <yoneyoneyone99@gmail.com>
2021-11-16 23:00:29 +0900

    パッケージのアップデート

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,10 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  stories: [
-  "../src/**/*.stories.mdx",
-  "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  addons: [
-  "@storybook/addon-links",
-  "@storybook/addon-essentials",
-  "storybook-addon-material-ui"
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   webpackFinal: (config) => {
-    config.resolve.alias['~'] = path.join(__dirname, '../src/');
+    config.resolve.alias["~"] = path.join(__dirname, "../src/");
     return config;
-  },
+  }
 };
