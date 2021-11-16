d2ee8257a328a4a349d3237378c9e161f57373e0
codyNog <yoneyoneyone99@gmail.com>
2021-11-17 02:45:28 +0900

    env設定を削除

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  env: {
-    SENTRY_DSN: process.env.SENTRY_DSN,
-    BASE_URL: process.env.BASE_URL
-  },
   webpack(config, options) {
     if (!options.isServer) {
       config.resolve.alias["@sentry/node"] = "@sentry/browser";
