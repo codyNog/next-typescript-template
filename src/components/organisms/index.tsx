e57318afd88bf827b2679c79256b21be8ca01073
codyNog <yoneyoneyone99@gmail.com>
2023-02-06 03:25:38 +0900

    zodの修正

--- src/components/organisms/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Molecule } from "~/components/molecules";
-
-export const Organism = (): JSX.Element => {
-  return <Molecule />;
-};
--- src/types/User/index.ts
+++ src/types/User/index.ts
@@ -1,12 +1,16 @@
 import { z } from "zod";
 
-const zodUser = z.object({ id: z.string(), name: z.string(), age: z.number() });
+export const userSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  age: z.number(),
+});
 
-export type User = z.infer<typeof zodUser>;
+export type User = z.infer<typeof userSchema>;
 
-const zodGetUsersParameter = z.object({
+export const getUsersParameterSchema = z.object({
   name: z.string().optional(),
   maxAge: z.number().optional(),
 });
 
-export type GetUsersParameter = z.infer<typeof zodGetUsersParameter>;
+export type GetUsersParameter = z.infer<typeof getUsersParameterSchema>;
