6fe0a8f22b806b8f3e909e0f345d36b5147a41f5
codyNog <yoneyoneyone99@gmail.com>
2023-03-01 13:16:19 +0900

    stroycapの設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "storycap"
   ],
   core: {
     builder: "@storybook/builder-vite",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import { withScreenshot } from 'storycap';
+
+export const decorators = [
+  withScreenshot, // Registration the decorator is required
+];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
