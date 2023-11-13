936ba34296f0f484252ffee77b6f89d7f3d91df2
codyNog <yoneyoneyone99@gmail.com>
2023-11-13 11:16:33 +0900

    plop修正

--- plopfile.cjs
+++ plopfile.cjs
@@ -1,7 +1,7 @@
 module.exports = function (plop) {
   // controller generator
-  plop.setGenerator("component", {
-    description: "react component",
+  plop.setGenerator("cc", {
+    description: "react client component",
     prompts: [
       // 入力させたい値につけたnameをactionsやtemplate内で参照できます
       {
@@ -14,7 +14,12 @@ module.exports = function (plop) {
       {
         type: "add",
         path: "src/components/{{pascalCase name}}/index.tsx",
-        templateFile: "plop-templates/component/index.tsx.hbs",
+        templateFile: "plop-templates/client-component/index.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/hooks/index.ts",
+        templateFile: "plop-templates/client-component/hooks/index.ts.hbs",
       },
       {
         type: "add",
@@ -26,10 +31,33 @@ module.exports = function (plop) {
         path: "src/components/{{pascalCase name}}/index.test.tsx",
         templateFile: "plop-templates/component/index.test.tsx.hbs",
       },
+    ],
+  });
+  plop.setGenerator("sc", {
+    description: "react server component",
+    prompts: [
+      // 入力させたい値につけたnameをactionsやtemplate内で参照できます
+      {
+        type: "input",
+        name: "name",
+        message: "component name please",
+      },
+    ],
+    actions: [
       {
         type: "add",
-        path: "src/components/{{pascalCase name}}/hooks/index.ts",
-        templateFile: "plop-templates/component/hooks/index.ts.hbs",
+        path: "src/components/{{pascalCase name}}/index.tsx",
+        templateFile: "plop-templates/server-component/index.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/index.stories.tsx",
+        templateFile: "plop-templates/component/index.stories.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/index.test.tsx",
+        templateFile: "plop-templates/component/index.test.tsx.hbs",
       },
     ],
   });
@@ -46,21 +74,21 @@ module.exports = function (plop) {
     actions: [
       {
         type: "add",
-        path: "src/pages/{{name}}/index.tsx",
+        path: "src/page-components/{{lowerCase name}}/index.tsx",
         templateFile: "plop-templates/page/index.tsx.hbs",
       },
       {
         type: "add",
-        path: "src/pages/{{name}}/index.stories.tsx",
+        path: "src/page-components/{{lowerCase name}}/index.stories.tsx",
         templateFile: "plop-templates/page/index.stories.tsx.hbs",
       },
       {
         type: "add",
-        path: "src/pages/{{name}}/hooks/index.ts",
+        path: "src/page-components/{{lowerCase name}}/hooks/index.ts",
         templateFile: "plop-templates/page/hooks/index.ts.hbs",
       },
       { type: "add",
-        path: "src/app/{{name}}/page.tsx",
+        path: "src/app/{{lowerCase name}}/page.tsx",
         templateFile: "plop-templates/page/page.tsx.hbs",
       }
     ],
