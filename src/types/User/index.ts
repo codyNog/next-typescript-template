10dae4a8b6e4a50513cf360dc9ac4a8e0ae55026
codyNog <yoneyoneyone99@gmail.com>
2023-11-14 17:11:08 +0900

    zodからvalibot置き換え

--- src/types/User/index.ts
+++ src/types/User/index.ts
@@ -1,22 +1,22 @@
-import { z } from "zod";
+import { Input, number, object, optional, string } from "valibot";
 
-export const userSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  age: z.number(),
+export const userSchema = object({
+  id: string(),
+  name: string(),
+  age: number(),
 });
 
 /**
  * User 概念の型定義
  **/
-export type User = z.infer<typeof userSchema>;
+export type User = Input<typeof userSchema>;
 
-export const getUsersParameterSchema = z.object({
-  name: z.string().optional(),
-  maxAge: z.number().optional(),
+export const getUsersParameterSchema = object({
+  name: optional(string()),
+  maxAge: optional(number()),
 });
 
 /**
  * User 一覧を取得するためのパラメータ
  **/
-export type GetUsersParameter = z.infer<typeof getUsersParameterSchema>;
+export type GetUsersParameter = Input<typeof getUsersParameterSchema>;
