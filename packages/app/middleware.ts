77db7e1bd8a74827b93095b9cd2ede596e42e2cc
codynog <yoneyoneyone99@gmail.com>
2024-09-29 10:32:30 +0900

    add auth

--- packages/app/middleware.ts
+++ packages/app/middleware.ts
@@ -1,19 +1,20 @@
 import { createI18nMiddleware } from "next-international/middleware";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { auth } from "./auth";
 
 const I18nMiddleware = createI18nMiddleware({
   locales: ["en", "ja"],
   defaultLocale: "en",
 });
 
-export function middleware(request: NextRequest) {
-  // I18nミドルウェアを先に適用
-  const i18nResponse = I18nMiddleware(request);
-
-  // セキュリティヘッダーとその他のヘッダーを追加
-  const response = i18nResponse || NextResponse.next();
-
+/**
+ * セキュリティヘッダーとその他のヘッダーを追加する
+ */
+const addHeaders = (
+  request: NextRequest,
+  response: NextResponse<unknown>,
+): NextResponse<unknown> => {
   // セキュリティヘッダー
   response.headers.set("X-XSS-Protection", "1; mode=block");
   response.headers.set("X-Frame-Options", "SAMEORIGIN");
@@ -55,7 +56,20 @@ export function middleware(request: NextRequest) {
   response.headers.set("Date", new Date().toUTCString());
 
   return response;
-}
+};
+
+export const middleware = async (request: NextRequest) => {
+  // 認証情報を取得
+  const _session = await auth();
+  // I18nミドルウェアを先に適用
+  const i18nResponse = I18nMiddleware(request);
+
+  // セキュリティヘッダーとその他のヘッダーを追加
+  const baseResponse = i18nResponse || NextResponse.next();
+  const response = addHeaders(request, baseResponse);
+
+  return response;
+};
 
 export const config = {
   matcher: ["/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)"],
